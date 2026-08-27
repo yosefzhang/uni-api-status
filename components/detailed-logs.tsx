9b2bac02ddb521cab9f0b027db62d22122de5aac
Yosef Zhang <yosefzhang@users.noreply.github.com>
2026-08-28 00:17:05 +0800

    style: 清理骨架屏注释

--- components/detailed-logs.tsx
+++ components/detailed-logs.tsx
@@ -275,13 +275,13 @@ export function DetailedLogs({ apiKey }: DetailedLogsProps) {
                   {[...Array(count)].map((_, i) => (
                       <TableRow key={i}>
                           <TableCell><Skeleton className="h-5 w-36" /></TableCell>
-                          <TableCell className="text-center"><Skeleton className="h-5 w-5 mx-auto rounded-full" /></TableCell> {/* Icon Skeleton */}
+                          <TableCell className="text-center"><Skeleton className="h-5 w-5 mx-auto rounded-full" /></TableCell>
                           <TableCell><Skeleton className="h-5 w-24" /></TableCell>
                           <TableCell><Skeleton className="h-5 w-24" /></TableCell>
                           <TableCell className="text-right"><Skeleton className="h-5 w-16 ml-auto" /></TableCell>
                           <TableCell className="text-right"><Skeleton className="h-5 w-16 ml-auto" /></TableCell>
                           <TableCell className="text-right"><Skeleton className="h-5 w-20 ml-auto" /></TableCell>
-                          <TableCell className="text-center"><Skeleton className="h-8 w-8 mx-auto" /></TableCell> {/* Button Skeleton */}
+                          <TableCell className="text-center"><Skeleton className="h-8 w-8 mx-auto" /></TableCell>
                       </TableRow>
                   ))}
               </TableBody>
