acabd1dd3da3c1bfcba92a83f40e91e2b94b8a06
Yosef Zhang <yosefzhang@users.noreply.github.com>
2026-08-28 00:17:05 +0800

    fix: 修复 hydration 警告

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="zh-CN">
+    <html lang="zh-CN" suppressHydrationWarning>
       <head>
         <link
           rel="icon"
